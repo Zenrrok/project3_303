a81da0c898564cfb71df2e0e44f127b23777a9b8
sretzke <stephanie.retzke@gmail.com>
2015-12-01 23:06:53 -0600

    Update BinaryTreeApp.cpp

--- Program3_303/Program3_303/BinaryTreeApp.cpp
+++ Program3_303/Program3_303/BinaryTreeApp.cpp
@@ -1,21 +1,31 @@
 
+#pragma once
 #include "Binary_Search_Tree.h"
+#include <string>
+#include <vector>
 using namespace std;
 
 
 
 int main() {
-	vector<std::string> BinarySearchData{ " ", "e", "i", "s", "h", "NULL", "NULL", "v", "NULL", "NULL", "u", "f", "NULL", "NULL", "Null", "a","r","l","NULL","NULL","NULL","w","p","NULL","NULL","j","NULL","NULL","t","n","d","b","NULL","NULL","x","NULL","NULL","k", "c", "NULL","NULL","y","NULL","NULL","m","g","z","NULL","NULL","q","NULL","NULL","o","NULL","NULL" };
-	
+	vector<string> BinarySearchData
+	{ " ", "e", "i", "s", "h", "NULL", "NULL", "v", "NULL", "NULL", "u", "f", "NULL", "NULL", "Null",
+	"a", "r", "l", "NULL", "NULL", "NULL", "w", "p", "NULL", "NULL", "j", "NULL", "NULL", "t", "n", "d",
+	"b", "NULL", "NULL", "x", "NULL", "NULL", "k", "c", "NULL", "NULL", "y", "NULL", "NULL", "m", "g", "z",
+	"NULL", "NULL", "q", "NULL", "NULL", "o", "NULL", "NULL" };
 
-	Binary_Search_Tree<std::string> binary_tree;
+
+	Binary_Search_Tree<string> binary_tree;
 	binary_tree.read_tree(BinarySearchData);
-	std::string target = "kept";
-	//std::string tobedeleted = "house";
+	string target = "01";
+
+	// "a" should be the output
 	binary_tree.find(target);
-	//binary_tree.erase(tobedeleted);
-	const std::string* maxNode = binary_tree.max();
-	const std::string* minNode = binary_tree.min();
+
+	system("PAUSE");
+
+	return 0;
+}
 	
 
 /*
@@ -29,6 +39,3 @@ For encoding I think a map will be a lot easier because the letter can be the ke
    
 
 	
-	
-
-}
